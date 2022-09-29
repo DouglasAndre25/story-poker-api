745837cc0ac2a273ff296fe925ce524d7923e572
KauanR <kau.matosr@gmail.com>
2022-09-29 19:28:45 -0300

    feat: increasing pagination size

--- src/app/helpers/pagination.js
+++ src/app/helpers/pagination.js
@@ -1,11 +1,11 @@
 exports.getPagination = (page, size) => {
-    const limit = size ? +size : 3
+    const limit = size ? +size : 10
     const offset = page ? page * limit : 0
   
     return { limit, offset }
 }
 
-exports.getPagingData = (totalItems, page, limit = 3) => {
+exports.getPagingData = (totalItems, page, limit = 10) => {
     
     const currentPage = page ? +page : 0
     const totalPages = Math.ceil(totalItems / limit)
